8ef86fb2d5392234cd51a78c8a246bf216ab8b1e
Gergő M <matyus.gergo@hmei.hu>
2021-09-09 12:18:46 +0200

    Part 11 - Specflow step definition implementation in .NET Core (Part 1)

---
+++ MessingAround/Steps/LoginSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MessingAround.Steps
+{
+	[Binding]
+	public class LoginSteps
+	{
+        [Given(@"I navigate to application")]
+        public void GivenINavigateToApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I enter username and password")]
+        public void GivenIEnterUsernameAndPassword(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I click login")]
+        public void GivenIClickLogin()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see user logged in to the application")]
+        public void ThenIShouldSeeUserLoggedInToTheApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
